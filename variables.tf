fcc569d0ce7c6629d0d0af0226d4fe07b7e5c6ee
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-13 00:06:39 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "organization_name" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "group_name" {
+  type = string
+}
+
+variable "api_token_group_name" {
+  type = string
+}
+
+variable "role" {
+  type = string
+}
