c83bc1dc69cec1227714cda073fa53f8e16e065a
Joey Lei <6957385+leizerbeam@users.noreply.github.com>
2024-04-13 00:03:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "hcp_terraform_project_group_access" { 
   source                = "./project_group_access"
+  organization_name     = var.organization_name
+  group_name            = var.group_name
+  project_name          = var.project_name
+  api_token_group_name  = var.api_token_group_name
+  role                  = var.role
 }
